544a0434fd9793c5439aa79ad3e9dea4fdb7cfcd
keyluxy <keyluxy@gmail.com>
2024-12-28 15:54:26 +0300

    init

---
+++ ELShopGabitov/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ELShopGabitov.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELShopGabitov.DatabaseContext
+{
+    class ApplicationDbContext : DbContext
+    {
+
+        DbSet<ProductEntity> Products { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductEntity>(productsConfigration =>
+            {
+                productsConfigration.HasKey(p => p.Id);
+            });
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;user=root;password=123456789;database=ELShopGabitov;",
+                new MySqlServerVersion(new Version(8, 0, 40)));
+        }
+    }
+}
